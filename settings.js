33d4e4103bb53874678a6c06335e61a4de13fd5b
DARK-SHADOW <124597615+MR-DARK-SHADOW@users.noreply.github.com>
2024-09-01 21:09:18 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,13 +5,13 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID,
-OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '' : process.env.OWNER_NUMBER,   
+SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=XYdFTIbT#vDao3BlMzoHUaXtDuYfDQt9hw6rRGFz83Kpp3jQgrJQ' : process.env.SESSION_ID,
+OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94751150234' : process.env.OWNER_NUMBER,   
 ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'false' : process.env.ONLY_GROUP,
 ONLY_ME: process.env.ONLY_ME === undefined ? 'false' : process.env.ONLY_ME,
 AUTO_STATUS_READ:  process.env.AUTO_STATUS_READ  || false  ,    
 PREFIX: process.env.PREFIX || '.' ,
-FOOTER: process.env.FOOTER=== undefined ? '‌👨‍💻 ᴠᴀᴊɪʀᴀ ᴍᴅ ʙʏ ᴛᴄ ᴛᴇᴀᴍ 👨‍💻': process.env.FOOTER,
+FOOTER: process.env.FOOTER=== undefined ? '‌ʙʏ-ᴅᴀʀᴋ-ꜱʜᴀᴏᴡ': process.env.FOOTER,
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgres://vajiratech_user:oSIFl2xmSojMZ0rkzdd0g0W6msuVTpNN@dpg-cpd7fjv109ks73e5gtig-a.frankfurt-postgres.render.com/vajiratech' : process.env.POSTGRESQL_URL,
 MAX_SIZE: 500, 
 ALIVE:  process.env.ALIVE  || ''  ,    
